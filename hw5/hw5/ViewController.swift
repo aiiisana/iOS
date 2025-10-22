6cc70d2d4780ba4825c82dde3187509fea47295c
aiiisana <aisanaond@gmail.com>
2025-10-23 01:19:01 +0500

    commit

--- hw5/hw5/ViewController.swift
+++ hw5/hw5/ViewController.swift
@@ -1,19 +1,38 @@
-//
-//  ViewController.swift
-//  hw5
-//
-//  Created by Aisana Ondassyn on 23.10.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flowerImageView: UIImageView!
+    @IBOutlet weak var flowerLabel: UILabel!
+    @IBOutlet weak var randomizeButton: UIButton!
+
+    let flowers = [
+        ("Chrysanthemum", UIImage(named: "Chrysanthemum")),
+        ("Dahlia", UIImage(named: "Dahlia")),
+        ("Freesia", UIImage(named: "Freesia")),
+        ("Gardenias", UIImage(named: "Gardenias")),
+        ("Hydrangea", UIImage(named: "Hydrangea")),
+        ("Orchid", UIImage(named: "Orchid")),
+        ("Peony", UIImage(named: "Peony")),
+        ("Poppies", UIImage(named: "Poppies")),
+        ("Rose", UIImage(named: "Rose")),
+        ("Spikenard", UIImage(named: "Spikenard"))
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        randomizeButton.layer.cornerRadius = 16
+        randomizeButton.backgroundColor = .systemPink
+        showRandomFlower()
     }
 
+    @IBAction func randomizeButtonTapped(_ sender: UIButton) {
+        showRandomFlower()
+    }
 
+    func showRandomFlower() {
+        let randomFlower = flowers.randomElement()!
+        flowerLabel.text = randomFlower.0
+        flowerImageView.image = randomFlower.1 ?? UIImage(systemName: "questionmark.circle")
+    }
 }
-
